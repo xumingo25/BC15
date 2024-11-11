349ab746a656cc085c0e9a26359a013c767e6617
Pobre Domingo <domingo.saavedra@tsoftglobal.com>
2024-11-11 12:03:33 -0300

    Se actualiza ejemplo con acciones de navegador y navegación

--- app/src/test/java/bc15/ejemploSelenium1.java
+++ app/src/test/java/bc15/ejemploSelenium1.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class ejemploSelenium1 {
 
     @Test
-    public void ejemplo1(){
+    public void ejemplo1() throws InterruptedException {
         String rutaDriver = System.getProperty("user.dir") +"\\src\\test\\resources\\drivers\\chromedriver.exe";
 
         //enlazar el webdriver a traves de property
@@ -17,6 +17,39 @@ public class ejemploSelenium1 {
 
         driver.get("https://www.google.com");
 
+        Thread.sleep(3000);
 
+        System.out.println("El titulo de la página web es: "+driver.getTitle());
+
+        System.out.println("La url actual es: "+ driver.getCurrentUrl());
+
+        System.out.println("DOM: "+driver.getPageSource());
+
+
+        driver.navigate().to("https://www.tsoftlatam.com");
+
+        Thread.sleep(3000);
+
+        System.out.println("El titulo de la página web es: "+driver.getTitle());
+
+        System.out.println("La url actual es: "+ driver.getCurrentUrl());
+
+        System.out.println("DOM: "+driver.getPageSource());
+
+        Thread.sleep(3000);
+
+        driver.navigate().back();
+
+        Thread.sleep(3000);
+
+        driver.navigate().forward();
+
+        Thread.sleep(3000);
+
+        driver.navigate().refresh();
+
+        Thread.sleep(3000);
+
+        driver.close();
     }
 }
