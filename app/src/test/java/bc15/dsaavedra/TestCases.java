912effadf4ec778313a7131fd7eef8482812ec92
Pobre Domingo <domingo.saavedra@tsoftglobal.com>
2024-11-12 11:44:38 -0300

    Se actualiza ejemplo con CP001

--- app/src/test/java/bc15/dsaavedra/TestCases.java
+++ app/src/test/java/bc15/dsaavedra/TestCases.java
@@ -1,5 +1,6 @@
 package bc15.dsaavedra;
 
+import bc15.dsaavedra.utils.Encoding;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -15,14 +16,40 @@ public class TestCases {
     @Test //crear test
     public void CP001_CreacionCta_Spotify() throws InterruptedException { // Thread.sleep
         driver.get("https://open.spotify.com/intl-es");
-        Thread.sleep(3000);
+        Thread.sleep(2000);
+
+        By byBtnRegistrarse = By.xpath("//button[@data-testid='signup-button']");
+
+        WebElement btnRegistrarse = driver.findElement(byBtnRegistrarse);
+
+        btnRegistrarse.click();
+
+        Thread.sleep(2000);
+
+        driver.findElement(By.id("username")).sendKeys("userbc1500001@gmail.com");
+
+        WebElement btnCerrarPopUp = driver.findElement(By.xpath("//button[@aria-label='Cerrar']"));
+
+        if(btnCerrarPopUp.isDisplayed()){
+            btnCerrarPopUp.click();
+        }
+
+        driver.findElement(By.xpath("//button[@data-testid='submit']")).click();
+
+        Thread.sleep(2000);
+
+        driver.findElement(By.name("new-password")).sendKeys("qwerty12345@");
+
+        Thread.sleep(2000);
+
+        driver.findElement(By.xpath("//button[@data-testid='submit']")).click();
+
     }
 
     @Test
     public void CP002_CreacionCta_SpotifyNOOK_MailVacio() throws InterruptedException {
         driver.get("https://open.spotify.com/intl-es");
         Thread.sleep(3000);
-
         By byBtnRegistrarse = By.xpath("//button[contains(text(),'Regis')]");
 
         WebElement btnRegistrarse = driver.findElement(byBtnRegistrarse);
@@ -41,7 +68,7 @@ public class TestCases {
 
         Thread.sleep(3000);
 
-        String resultadoEsperado = "Este correo electrónico no es válido. Asegúrate de que tenga un formato como este: ejemplo@email.com";
+        String resultadoEsperado = Encoding.corregirEncoding("Este correo electrónico no es válido. Asegúrate de que tenga un formato como este: ejemplo@email.com");
         String resultadoActual = driver.findElement(By.xpath("//span[contains(text(),'Este correo')]")).getText();
 
         Assertions.assertEquals(resultadoEsperado,resultadoActual);
@@ -65,4 +92,6 @@ public class TestCases {
     public void posCondiciones(){
         //driver.close();
     }
+
+
 }
