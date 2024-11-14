61f1c1df627cb487372302e00601a0826b9180e8
BenjaMontero <benjamin.montero@tsoftglobal.com>
2024-11-14 16:28:10 -0300

    merge

--- app/src/test/java/bc15/BenjaMontero/unidad2/BCI_BenjaminMontero.java
+++ app/src/test/java/bc15/BenjaMontero/unidad2/BCI_BenjaminMontero.java
@@ -12,7 +12,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class BCI {
+public class BCI_BenjaminMontero {
 
     private WebDriver driver;
     private WebDriverWait wait;
